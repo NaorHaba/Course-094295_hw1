aad3e789e5e512366b66f7fe7dee251c5d98ea9c
nivsfa <63532632+nivsfa@users.noreply.github.com>
2022-05-06 14:55:26 +0300

    Update train_validate.py

--- train_validate.py
+++ train_validate.py
@@ -3,12 +3,13 @@ import pandas as pd
 import numpy as np
 import torch.nn as nn
 import torch
+import wandb
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
 import random
 
 from score_functions import F1
 from LSTMTrainer import RNNTrainer
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, WeightedRandomSampler
 
 from patient_LSTM import TrainDataset, patientLSTM, batch_collate
 random.seed(42)
@@ -34,30 +35,77 @@ def split_train_validation(valid_size):
     return train, valid
 
 
+def under_sample(train, under_sample_rate):
+    healthy = set(train[train['SepsisLabel'] == 0.0]['id'].unique())
+    remove_healthy = random.sample(healthy, (1 - under_sample_rate) * len(healthy))
+    train = train[~train['id'].isin(remove_healthy)]
+    return train
+
+
+def weighted_over_sampler(train):
+    samples_weight = []
+    healthy = set(train[train['SepsisLabel'] == 0.0]['id'].unique())
+    sick = set(train[train['SepsisLabel'] == 1.0]['id'].unique())
+    for patient, label in train.groupby('id')['SepsisLabel'].max():
+        if label == 1.0:
+            samples_weight.append(1. / len(sick))
+        else:
+            samples_weight.append(1. / len(healthy))
+    samples_weight = torch.tensor(samples_weight)
+    sampler = WeightedRandomSampler(samples_weight, len(samples_weight))
+
+    return sampler
+
+
+def scaler_fn(scaler, scaling_columns, train, valid, test):
+    train[scaling_columns] = scaler.fit_transform(train[scaling_columns])
+    valid[scaling_columns] = scaler.transform(valid[scaling_columns])
+    test[scaling_columns] = scaler.transform(test[scaling_columns])
+    return train, valid, test
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Train and Validate model')
 
     # Paths
-    parser.add_argument('train_file', type=str, help='path to train csv file')
-    parser.add_argument('test_file', type=str, help='path to test csv file')
-    parser.add_argument('valid_size', type=float, help='size of validation data (proportion)')
+    parser.add_argument('train_file', type=str, help='path to train csv file',
+                        default='data/train_raw.csv')
+    parser.add_argument('test_file', type=str, help='path to test csv file',
+                        default='data/test_raw.csv')
+    parser.add_argument('valid_size', type=float, help='size of validation data (proportion)',
+                        default=0.15)
     # Data parameters
-    parser.add_argument('sampling_method', type=str, help='method for sampling')
-    parser.add_argument('remove_columns', type=str, help='method for columns removal')
-    parser.add_argument('scale_method', type=str, help='method for scaling')
+    parser.add_argument('sampling_method', type=str, help='method for sampling',
+                        default='under')
+    parser.add_argument('under_sample_rate', type=float, help='sampling rate for under-sampling',
+                        default=0.5)
+    parser.add_argument('remove_columns', type=str, help='method for columns removal',
+                        default='')
+    parser.add_argument('scale_method', type=str, help='method for scaling',
+                        default='standard')
+    parser.add_argument('scaling_columns', type=str, help='method for columns removal',
+                        default='')  # '_' delimited: "HR_Temp_..."
     # Model parameters
-    parser.add_argument('batch_size', type=str, help='batch size')
-    parser.add_argument('train_batch_size', type=float, help='train batch size')
-    parser.add_argument('test_batch_size', type=float, help='test batch size')
-    parser.add_argument('window_size', type=int, help='window size')
-    parser.add_argument('loss_fn', type=str, help='loss function for model')
-    parser.add_argument('optimizer', type=str, help='optimizer for model')
-    parser.add_argument('hidden_dim', type=int, help='')
-    parser.add_argument('LSTM_n_layers', type=int, help='')
-    parser.add_argument('dropout', type=float, help='')
-    parser.add_argument('lr', type=float, help='')
-    parser.add_argument('true_threshold', type=float, help='')
-    parser.add_argument('epochs', type=int, help='')
+    parser.add_argument('batch_size', type=float, help='batch size',
+                        default=30)
+    parser.add_argument('train_batch_size', type=float, help='train batch size',
+                        default=30)
+    parser.add_argument('test_batch_size', type=float, help='test batch size',
+                        default=30)
+    parser.add_argument('window_size', type=int, help='window size',
+                        default=30)
+    parser.add_argument('hidden_dim', type=int, help='',
+                        default=600)
+    parser.add_argument('LSTM_n_layers', type=int, help='',
+                        default=3)
+    parser.add_argument('dropout', type=float, help='',
+                        default=0.4)
+    parser.add_argument('lr', type=float, help='',
+                        default=0.01)
+    parser.add_argument('true_threshold', type=float, help='',
+                        default=0.5)
+    parser.add_argument('epochs', type=int, help='',
+                        default=100)
 
     args = parser.parse_args()
 
@@ -76,13 +124,13 @@ if __name__ == '__main__':
     sampler = None
     # Under/Over-Sampling
     if args.sampling_method is not None:
-        # TODO change to different functions
-        if args.sampling_method == 'sample_health_records':
-            pass
-        remove_amount = len(t_healthy) - 2 * len(t_sick)
-        # remove_amount = 5000
-        remove_healthy = random.sample(healthy, remove_amount)
-        train = train[~train['id'].isin(remove_healthy)]
+        if 'under' in args.sampling_method:
+            train = under_sample(train, under_sample_rate=args.under_sample_rate)
+        elif 'over' in args.sampling_method:
+            sampler = weighted_over_sampler(train)
+        else:
+            train = under_sample(train, under_sample_rate=args.under_sample_rate)
+            sampler = weighted_over_sampler(train)
 
     # Remove columns
     if args.remove_columns is not None:
@@ -93,13 +141,16 @@ if __name__ == '__main__':
 
     # scale columns
     if args.scale_method is not None:
-        # TODO change to different functions
-        scaling_columns = ['HR', 'O2Sat', 'Temp', 'SBP', 'MAP', 'DBP', 'Resp', 'EtCO2', 'Age', 'HospAdmTime',
-                           'ICULOS']  # rest are already scaled
-        scaler = MinMaxScaler()
-        train[scaling_columns] = scaler.fit_transform(train[scaling_columns])
-        valid[scaling_columns] = scaler.transform(valid[scaling_columns])
-        test[scaling_columns] = scaler.transform(test[scaling_columns])
+        if args.scale_method == 'standard':
+            scaler = StandardScaler()
+        else:
+            scaler = MinMaxScaler()
+        if not args.scaling_columns:
+            scaling_columns = ['HR', 'O2Sat', 'Temp', 'SBP', 'MAP', 'DBP', 'Resp', 'EtCO2', 'Age', 'HospAdmTime',
+                               'ICULOS']  # rest are already scaled
+        else:
+            scaling_columns = args.scaling_columns.split('_')
+        train, valid, test = scaler_fn(scaler, scaling_columns, train, valid, test)
 
     # create data loaders
     train_X, train_y = split_features_label(train)
@@ -128,7 +179,11 @@ if __name__ == '__main__':
     trainer = RNNTrainer(model, loss_fn, optimizer, true_threshold=args.true_threshold)
 
     # train and test
-    trainer.fit(train_dl, valid_dl, args.epochs, score_fn=score_fn, checkpoints=f'models/LSTM_window{args.window_size}',
+    model_name = f'{wandb.run.dir}/model'
+    trainer.fit(train_dl, valid_dl, args.epochs, score_fn=score_fn, checkpoints=model_name,
                 early_stopping=15)
 
+    trainer.model.load_state_dict(torch.load(f'{model_name}.pth'))
+
     trainer.test(test_dl, score_fn=score_fn)
+    
